8aaaa923a86a61d0b0951d4b37834b6f06a1fc21
WMs784 <wms784.app@gmail.com>
2022-05-06 14:05:25 +0900

    Added json format of api data

---
+++ app/src/main/java/com/example/searchphoto/ApiJson.kt
@@ -0,0 +1,36 @@
+package com.example.searchphoto
+
+data class TweetData(
+    val `data`: List<Data>,
+    val includes: Includes
+)
+data class Data(
+    val attachments: Attachments,
+    val author_id: String,
+    val created_at: String,
+    val id: String,
+    val public_metrics: PublicMetrics,
+    val source: String,
+    val text: String
+)
+data class Attachments(
+    val media_keys: List<String>
+)
+data class Includes(
+    val media: List<Media>
+)
+data class Media(
+    val duration_ms: Int,
+    val media_key: String,
+    val public_metrics: PublicMetricsX,
+    val type: String
+)
+data class PublicMetricsX(
+    val view_count: Int
+)
+data class PublicMetrics(
+    val like_count: Int,
+    val quote_count: Int,
+    val reply_count: Int,
+    val retweet_count: Int
+)
